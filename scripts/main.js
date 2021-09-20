6cf915d65977a776943c1f41c6c739d2bf294956
danielmusau <danielmusau.dm19@gmail.com>
2021-09-20 13:20:35 +0300

    Practice assignment 1

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+//Practice Assignment 1
+//Question 1
+let myNumber;
+
+//Question 2
+myNumber = 5;
+console.log(myNumber + " is a " + typeof myNumber);
+
+
+//Question 3
+let myOtherNumber;
+myOtherNumber = 5;
+console.log(myNumber+ " is a " + typeof myOtherNumber);
+
+//Question 4
+let myTotal;
+myTotal = myNumber * myOtherNumber;
+console.log("Total product is " +myTotal+ " which is a " + typeof myTotal);
+
+//Question 5
+let myName;
+myName = "Veasna";
+console.log(myName + " is my name which is a type of "+ typeof myName);
+
+let myCombo;
+myCombo = "10";
+console.log("The sum is " +myCombo+ " which is a " +typeof myCombo);
+
+//Question 6
+let myOtherCombo;
+myOtherCombo = myNumber + myOtherNumber;
+
+console.log("The sum is " +myOtherCombo+ " which is a " +typeof myOtherCombo);
+
+//Question 7
+let myRemainder;
+myRemainder = myNumber % 5;
+
+console.log("The remainder of dividing 5 by 5 is "+myRemainder+ " which is a " +typeof myRemainder);
+
+//Question 8
+let i;
+i = 2;
+console.log(i +" is a " +typeof i);
+
+//Question 9
+let int;
+int = 4;
+console.log(int + " is a " +typeof int);
+
+//Question 10
+let myBoolean;
+myBoolean = false;
+console.log(myBoolean + " is a type of " +typeof myBoolean);
